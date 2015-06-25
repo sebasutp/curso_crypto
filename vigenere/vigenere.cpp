6c6bf02cc53f6aacfd04309cf4c6bbaced33d639
sebasutp <sebasutp@gmail.com>
2015-06-25 13:06:52 -0500

    Correction to vigenere code to accept utf8 input

--- vigenere/vigenere.cpp
+++ vigenere/vigenere.cpp
@@ -5,9 +5,26 @@
 #include <vector>
 #include <map>
 #include <boost/program_options.hpp>
+#include <utf8.h>
 
 using namespace std;
 using namespace boost::program_options;
+
+utf8::unchecked::iterator<string::iterator> utf8_begin(string &str) {
+  return utf8::unchecked::iterator<string::iterator>(str.begin());
+}
+
+utf8::unchecked::iterator<string::const_iterator> utf8_begin(const string &str) {
+  return utf8::unchecked::iterator<string::const_iterator>(str.begin());
+}
+
+utf8::unchecked::iterator<string::iterator> utf8_end(string &str) {
+  return utf8::unchecked::iterator<string::iterator>(str.end());
+}
+
+utf8::unchecked::iterator<string::const_iterator> utf8_end(const string &str) {
+  return utf8::unchecked::iterator<string::const_iterator>(str.end());
+}
 
 struct SebException : exception {
   string msg;
@@ -22,32 +39,35 @@ struct SebException : exception {
 };
 
 class Mapper {
-  map<char,int> forward;
-  map<int,char> backward;
+  map<uint32_t,uint32_t> forward;
+  map<uint32_t,uint32_t> backward;
 
 public:
 
   Mapper(const string& filename) {
     ifstream file(filename.c_str());
-    char c;
+    uint32_t chr;
     string token;
     for (int i = 0; file >> token; i++) {
-      if (token.size() == 1) {
-        c = token[0];
+      if (utf8::distance(token.begin(), token.end()) == 1) {
+        chr = utf8::peek_next(token.begin(),token.end());
       } else if (token == "space") {
-        c = ' ';
+        chr = ' ';
       } else if (token == "\\n") {
-        c = '\n';
+        chr = '\n';
       } else {
         throw SebException("alphabet token not recognized: " + token);
       }
-      forward[c] = i;
-      backward[i] = c;
+      if (forward.count(chr) > 0) {
+        throw SebException("The symbol " + token + " appears morea than once in the alphabet");
+      }
+      forward[chr] = i;
+      backward[i] = chr;
     }
     file.close();
   }
 
-  bool contains(char x) const {
+  bool containsChr(uint32_t x) const {
     return forward.count(x) > 0;
   }
 
@@ -55,34 +75,42 @@ public:
     return forward.size();
   }
 
-  int charToInt(char x) const {
+  uint32_t charToInt(uint32_t x) const {
     return forward.find(x)->second;
   }
 
-  char intToChar(int x) const {
+  uint32_t intToChar(uint32_t x) const {
     return backward.find(x)->second;
   }
 };
 
 string encrypt(const string& plain, const string& key, const Mapper& m) {
   string cipher;
-  for (int i=0; i<plain.size(); i++) {
-    int charId = m.charToInt(plain[i]);
-    int keyId = m.charToInt(key[i % key.size()]);
+  vector<uint32_t> ucipher;
+  for (auto tchr = utf8_begin(plain), kchr=utf8_begin(key);
+      tchr != utf8_end(plain); tchr++, kchr++) {
+    if (kchr == utf8_end(key)) kchr = utf8_begin(key);
+    uint32_t charId = m.charToInt(*tchr);
+    uint32_t keyId = m.charToInt(*kchr);
     int cipherId = (charId + keyId) % m.size();
-    cipher.push_back(m.intToChar(cipherId));
+    ucipher.push_back(m.intToChar(cipherId));
   }
+  utf8::utf32to8(ucipher.begin(),ucipher.end(),back_inserter(cipher));
   return cipher;
 }
 
 string decrypt(const string& cipher, const string& key, const Mapper& m) {
   string plain;
-  for (int i=0; i<cipher.size(); i++) {
-    int cipherId = m.charToInt(cipher[i]);
-    int keyId = m.charToInt(key[i % key.size()]);
-    int plainId = (cipherId - keyId + m.size()) % m.size();
-    plain.push_back(m.intToChar(plainId));
+  vector<uint32_t> uplain;
+  for (auto tchr = utf8_begin(cipher), kchr=utf8_begin(key);
+      tchr != utf8_end(cipher); tchr++, kchr++) {
+    if (kchr == utf8_end(key)) kchr = utf8_begin(key);
+    uint32_t charId = m.charToInt(*tchr);
+    uint32_t keyId = m.charToInt(*kchr);
+    int cipherId = (charId - keyId + m.size()) % m.size();
+    uplain.push_back(m.intToChar(cipherId));
   }
+  utf8::utf32to8(uplain.begin(),uplain.end(),back_inserter(plain));
   return plain;
 }
 
@@ -120,9 +148,9 @@ int main(int argc, char **argv) {
     }
     if (text.back() == '\n') text.pop_back();
     if (!ignore_unknown) {
-      for (auto ch : text) {
-        if (!m.contains(ch)) {
-          cerr << "The character " << ch << " of the input text is not in the alphabet" << endl;
+      for (auto ch = utf8_begin(text); ch != utf8_end(text); ch++) {
+        if (!m.containsChr(*ch)) {
+          cerr << "The character Unicode=" << *ch << " of the input text is not in the alphabet" << endl;
           return 1;
         }
       }
